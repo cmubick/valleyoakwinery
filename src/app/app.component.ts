f2958bd3c25edf4e134812963ada482780243cb4
cubick <christopher.ubick@bytecubed.com>
2018-08-25 15:55:53 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vow-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'valleyoakwinery';
+}
